1b41f9421afb4015dbf1cb43925e039b264b6447
matterwoven <145516753+matterwoven@users.noreply.github.com>
2023-10-13 18:21:48 -0700

    Add project files.

---
+++ Code View y Msg Box/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Code_View_y_Msg_Box
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MessageBox.Show("Hello Class! \n" +
+                "Today we're learning about Computer Science and It's applications in real life");
+
+            //end
+        }
+    }
+}
